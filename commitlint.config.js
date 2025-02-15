8d46b0eaf828abd65d702863de4707b49e7d12c9
ngocduong <duong2lophot@gmail.com>
2025-02-15 09:38:22 +0700

    fix: Resolve conflict

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	extends: ['@commitlint/config-conventional'],
-
 	rules: {
 		'type-empty': [2, 'never'],
 		'type-enum': [
@@ -21,5 +20,8 @@ module.exports = {
 			],
 		],
 		'subject-case': [2, 'always', ['sentence-case']],
+		'type-case': [2, 'always', ['sentence-case']],
+		'header-max-length': [2, 'always', 72],
+		'header-min-length': [2, 'always', 0],
 	},
 };
